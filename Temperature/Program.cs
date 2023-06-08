c8237a80753ca9dccee806c7bd02ac12b053846b
DonV1K <donvikt1@gmail.com>
2023-06-08 19:34:30 +0300

    Temperature

---
+++ Temperature/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите температуру");
+
+var temperature = Convert.ToInt32(Console.ReadLine());
+
+switch (temperature)
+{
+        case (< 0):
+        Console.WriteLine("Очень холодно");
+        break;
+
+        case (>= 0) and (< 10):
+        Console.WriteLine("Холодно");
+        break;
+
+    case (>= 10) and (< 20):
+        Console.WriteLine("Нормально");
+        break;
+
+    case (>= 20) and (< 30):
+        Console.WriteLine("Тепло");
+        break;
+
+    case (>= 30):
+        Console.WriteLine("Жарко");
+        break;
+
+}
