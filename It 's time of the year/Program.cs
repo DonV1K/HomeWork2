5dd3bbbccae08d51898a3d87d7c886ac10410a6f
DonV1K <donvikt1@gmail.com>
2023-06-08 19:55:19 +0300

    It 's time of the year

---
+++ It 's time of the year/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите текущий месяц");
+
+var month = Console.ReadLine();
+
+switch (month)
+{
+    case ("Декабрь") or ("Январь") or ("Февраль"):
+        Console.WriteLine("Зима");
+        break;
+
+    case ("Март") or ("Апрель") or ("Май"):
+        Console.WriteLine("Весна");
+        break;
+
+    case ("Июнь") or ("Июль") or ("Август"):
+        Console.WriteLine("Лето");
+        break;
+
+    case ("Сентябрь") or ("Октябрь") or ("Ноябрь"):
+        Console.WriteLine("Осень");
+        break;
+
+        default:
+        Console.WriteLine("null");
+        break;
+
+}
