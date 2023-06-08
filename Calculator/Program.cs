a4b896fe43e66b6181ead049ac103fee88d38195
DonV1K <donvikt1@gmail.com>
2023-06-08 18:30:04 +0300

    Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите первое число");
+var firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите знак операции");
+var sign = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+var secondNumber = Convert.ToInt32(Console.ReadLine());
+
+switch (sign)
+{
+    case "+":
+        Console.WriteLine(firstNumber + secondNumber);
+        break;
+
+    case "-":
+        Console.WriteLine(firstNumber - secondNumber);
+        break;
+
+    case "/":
+            Console.WriteLine(firstNumber / secondNumber);
+        break;
+
+    case "*":
+        Console.WriteLine(firstNumber * secondNumber);
+        break;
+
+    case "%":
+        Console.WriteLine(firstNumber % secondNumber);
+        break;
+
+        default:
+        Console.WriteLine("null");
+        break;
+
+}
+
