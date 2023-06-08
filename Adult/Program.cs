1c2eb26aade63b306e88e51ecdd7794cd98f2912
DonV1K <donvikt1@gmail.com>
2023-06-08 18:44:32 +0300

    Adult

---
+++ Adult/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите ваш возраст");
+
+var age = Convert.ToInt32(Console.ReadLine());
+
+var isAdult = age >= 18;
+
+if (isAdult == true)
+{
+    Console.WriteLine("Совершеннолетний");
+}
+else
+{
+    Console.WriteLine("Не совершеннолетний");
+}
