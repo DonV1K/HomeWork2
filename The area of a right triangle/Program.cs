f826e6438dd61d23b928ea34134e7df4df814343
DonV1K <donvikt1@gmail.com>
2023-06-08 18:08:39 +0300

    The area of a right triangle

---
+++ The area of a right triangle/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите сторону а");
+
+var side1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите сторону b");
+
+var side2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Площадь прямоугольного треугольника = {side1 * side2 / 2}");
